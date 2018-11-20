3ea1be937411360e3c0bddd808493dc1a7b8b2ea
Josh Pharaoh <44850838+jpharaoh27@users.noreply.github.com>
2018-11-20 20:55:17 +1300

    Intro. to JS

---
+++ 1 - Dog Years.js
@@ -0,0 +1,14 @@
+// My age
+const myAge = 26
+// Early years
+let earlyYears = 2 * 10.5
+// Doggys later years (after 2 yrs old)
+let laterYears = myAge - 2
+//Later Years
+laterYears *= 4
+//My Age in Doggy years
+let myAgeInDogYears = earlyYears + laterYears
+//My Name!
+const myName = 'JOSH'.toLowerCase()
+//output
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in puppy years!`)
